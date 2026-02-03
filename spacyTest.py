d4cccc8b1d53e541606262005e42bcf393e4cf07
Cleberson <cleber4557@gmail.com>
2026-02-03 13:08:43 -0300

    testando a lib spaCy

--- spacyTest.py
+++ spacyTest.py
@@ -5,3 +5,26 @@ doc = nlp("Boa tarde, como estão as coisas ai? Já terminou o que estava fazend
 
 for token in doc:
     print(token.text, token.lemma_, token.pos_)
+
+
+# Vou usar essa lib para ajudar na verificação de cada intent do usuário, o papel dela aqui foi destrinchar essa frase em tokens
+# Resultado Obtido:
+# Boa Boa ADJ  -- 
+# tarde tarde ADV
+# , , PUNCT
+# como como ADV
+# estão estar VERB
+# as o DET
+# coisas coisa NOUN
+# ai ai ADJ
+# ? ? PUNCT
+# Já já ADV
+# terminou terminar VERB
+# o o PRON
+# que que PRON
+# estava estar AUX
+# fazendo fazer VERB
+# ? ? PUNCT
+
+
+#text == palavra da frase / lemma == lemantização, palavra crua do dicionário / Pos == classe gramatical (part of speach)
